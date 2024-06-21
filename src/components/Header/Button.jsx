39ca21b467f8fe86f42494a5cd33876820d98483
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2024-06-21 22:32:22 +0500

    update 2 files and create 1 file: add index.js, Login.jsx and Button.jsx

--- src/components/Header/Button.jsx
+++ src/components/Header/Button.jsx
@@ -5,12 +5,12 @@ const Button = ({
   textColor = "text-white",
   bgColor = "bg-blue-500",
   hover = "hover:bg-blue-700",
-  classname = "",
+  className = "", // Corrected the spelling of 'className'
   ...props
 }) => {
   return (
     <button
-      className={`px-4 py-2 rounded transition duration-200 ${textColor} ${bgColor} ${hover} ${className}`}
+      className={`px-4 py-2 rounded transition duration-200 ${textColor} ${bgColor} ${hover} ${className}`} // Changed 'classname' to 'className'
       {...props}
     >
       {children}
--- src/components/index.js
+++ src/components/index.js
@@ -11,4 +11,7 @@ export{
     Logo,
     Container,
     LogoutBtn
+
+    
 }
+
