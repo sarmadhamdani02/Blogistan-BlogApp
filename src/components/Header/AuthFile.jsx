5033afd10da133b5ce1aa5e58f6abac9a571fd18
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2024-06-22 20:51:12 +0500

    update AuthFile.jsx

--- src/components/Header/AuthFile.jsx
+++ src/components/Header/AuthFile.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const AuthFile = () => {
-  return (
-    <div>AuthFile</div>
-  )
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+
+  return loader ? <div>Loading...</div> : <>{children}</>;
 }
-
-export default AuthFile
